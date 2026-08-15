c6f1a5b0c85310ec8f7dbdfe03bb8c20ebf8ee3d
P1GUM1 <akinosyoe@gmail.com>
2026-08-16 07:38:06 +0900

    fix: コメントの記載

---
+++ Application/main.cpp
@@ -0,0 +1,15 @@
+/*
+* テンプレート
+* C/C++を使用したWindowsアプリケーションの基本的なテンプレート
+* ISO C++20 準拠
+* Application/main.cpp
+* 最終更新日: 2026/08/16
+*/
+
+
+#include "Windows.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+}
