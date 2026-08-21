723552129759ff84845ba6f1a1b489b80f4fed69
P1GUM1 <akinosyoe@gmail.com>
2026-08-22 04:26:04 +0900

    feat: 処理の明確化LocalLLMのSample削除

--- Application/main.cpp
+++ Application/main.cpp
@@ -8,92 +8,91 @@
 
 #include "Window.h"
 #include "DirectX12.h"
-#include "BaseLLM.h"
 
 // Windowsアプリケーションのエントリーポイント
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	/* Windowの初期化 */
 	Window* window = Window::GetInstance();
-	window->SetWindowTitle("Template");
-	window->SetWindowWidth(720);
-	window->SetWindowHeight(480);
-	window->Initialize(hInstance, nCmdShow);
-
-	HWND Sub = window->CreateSubSeparateWindow(1, "Sub Window", 1000, 100, 400, 900);
-	window->CreateTextBox(2, "", 5, 10, 370, 700, Sub);
-	window->CreateRichEdit(3, "", 5, 720, 300, 100, Sub);
-	window->CreateButton(4, "Send", 310, 720, 60, 100, Sub);
-
-
-	/* DirectX12の初期化 */
 	DirectX::DirectX12* directX12 = DirectX::DirectX12::Get();
-	directX12->Init(window->GetMainWindowHandle(), window->GetClientWidth(), window->GetClientHeight());
-
-	/* Descriptorの初期化 */
 	DirectX::Descriptor* descriptor = DirectX::Descriptor::Get();
-	descriptor->Init();
 
-
-	BaseLLM* llm = new BaseLLM("Model/Qwen3.5-4B-UD-Q4_K_XL.gguf", nullptr, 99, 4096, 512, 1024);
-	
-
-	/* 基礎ループ */
-	MSG msg = {};
-
-	bool isRunning = false;
-
-	try
+	/* 初期化 */
 	{
-		while (true)
+		/* Windowの初期化 */
+		window->SetWindowTitle("Template");
+		window->SetWindowWidth(720);
+		window->SetWindowHeight(480);
+		window->Initialize(hInstance, nCmdShow);
+
+		HWND Sub = window->CreateSubSeparateWindow(1, "Sub Window", 1000, 100, 400, 900);
+		window->CreateTextBox(2, "", 5, 10, 370, 700, Sub);
+		window->CreateRichEdit(3, "", 5, 720, 300, 100, Sub);
+		window->CreateButton(4, "Send", 310, 720, 60, 100, Sub);
+
+		/* DirectX12の初期化 */
+		directX12->Init(window->GetMainWindowHandle(), window->GetClientWidth(), window->GetClientHeight());
+
+		/* Descriptorの初期化 */
+		descriptor->Init();
+	}
+
+	/* ループ処理 */
+	{
+		/* 基礎ループ */
+		MSG msg = {};
+
+		bool isRunning = false;
+
+		try
 		{
-			if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+			while (true)
 			{
-				if (msg.message == WM_QUIT)
+				if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 				{
-					break;
+					if (msg.message == WM_QUIT)
+					{
+						break;
+					}
+					TranslateMessage(&msg);
+					DispatchMessageA(&msg);
 				}
-				TranslateMessage(&msg);
-				DispatchMessageA(&msg);
-			}
-			else
-			{
-				// 更新処理
-
-				if(window->IsButtonClicked(4))
+				else
 				{
-					std::string prompt = window->GetEditText(3);
-					window->SetBoxText(3, "");
-					window->AddTextBoxText(2, "User: " + prompt + "\r\n");
+					// 更新処理
 
-					window->AddTextBoxText(2, "LLM: " + Helper::Utf8ToAnsi(llm->ProcessPrompt(Helper::AnsiToUtf8(prompt))) + "\r\n");
-					isRunning = true;
+					if (window->IsButtonClicked(4))
+					{
+
+					}
+
+
+
+					// 描画処理
+					directX12->BeginDraw();
+
+					// ここに描画処理を追加する
+
+					directX12->EndDraw();
+					directX12->ApplyResizeIfNeeded();
+
 				}
-			
-
-
-				// 描画処理
-				directX12->BeginDraw();
-
-				// ここに描画処理を追加する
-
-				directX12->EndDraw();
-				directX12->ApplyResizeIfNeeded();
-
 			}
 		}
-	}
-	catch (const std::exception& e)
-	{
-		MessageBoxA(nullptr, e.what(), "Error", MB_OK | MB_ICONERROR);
-	}
-	catch (...)
-	{
-		MessageBoxA(nullptr, "Unknown error occurred.", "Error", MB_OK | MB_ICONERROR);
+		catch (const std::exception& e)
+		{
+			MessageBoxA(nullptr, e.what(), "Error", MB_OK | MB_ICONERROR);
+		}
+		catch (...)
+		{
+			MessageBoxA(nullptr, "Unknown error occurred.", "Error", MB_OK | MB_ICONERROR);
+		}
 	}
 
-	descriptor->Del();
-	directX12->Del();
-	window->DestroyInstance();
+	/* 終了処理 */
+	{
+		descriptor->Del();
+		directX12->Del();
+		window->DestroyInstance();
+	}
 	return 0;
 }
